db5c0613902001a701ff3e1494cc337d664bb129
sami <shahsamiksha54@gmail.com>
2025-01-15 11:32:36 +0000

    auth modal compled

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,36 +1,96 @@
+import { ImCross } from "react-icons/im";
 
-import MyLogo from "../assets/logo.png";
+export default function FlipCard(props) {
+  const HideShow = () => {
+    props.setCrossBtn(false);
+  };
 
-export default function FlipCard() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-200">
-            <div className="flip-container perspective">
-                <div className="flip-card">
-                    {/* Front Side */}
-                    <div className="front w-full h-full flex flex-col justify-center items-center py-8 gap-6 bg-white rounded-lg shadow-md absolute backface-hidden">
-                        <p className="text-[0.6rem] font-bold">Sumu Clothing</p>
-                        <div className="h-[75px] w-[75px]">
-                            <img src={MyLogo} alt="logo" />
-                        </div>
-                        <p className="text-[0.6rem]">Have an Account?</p>
-                        <button className="text-center text-[0.6rem] border-2 rounded-lg border-[purple] px-3 py-1">
-                            Login
-                        </button>
-                    </div>
+  return (
+    <div
+      className={`min-h-screen  ${
+        props.crossBtn ? " block" : "hidden"
+      }  bg-[rgba(0,0,0,0.6)] fixed top-0 left-0 w-full h-full flex justify-center items-center`}
+    >
+      <div
+        className={`flip-container   perspective relative w-[90%] max-w-[600px]`}
+      >
+        <div className={`flip-card`}>
+          {/* Front Side */}
+          <div className="front w-full h-full flex flex-col justify-center items-center gap-6 bg-white rounded-lg shadow-md  backface-hidden">
+            <p className="text-[0.6rem] font-bold">Sumu Clothing</p>
+            <form className="flex flex-col w-[90%] items-center ">
+              <input
+                type="text"
+                placeholder="Enter your Email"
+                className=" text-[0.8rem] w-full border-2 border-[var(--primary-bg-color)] py-1 pl-4 rounded-md"
+              />
+              <input
+                type="text"
+                placeholder="password"
+                className="border-2 text-[0.8rem] w-full border-[var(--primary-bg-color)] py-1 pl-4 rounded-md"
+              />
+              <div className="flex gap-4 py-4 item-center">
+                <input type="checkbox" className="h-5 w-5" />
+                <p className="text-[0.8rem]">Remember me</p>
+              </div>
+              <button
+                type="submit"
+                className="text-center text-[0.6rem] border-2 rounded-lg border-[purple] px-3 py-1"
+              >
+                login
+              </button>
+            </form>
+            <p className="text-[0.6rem]">Don't Have an Account?</p>
+            <button className="text-center text-[0.6rem] border-2 rounded-lg border-[purple] px-3 py-1">
+              Signup
+            </button>
+            <button
+              onClick={HideShow}
+              className="absolute top-0 right-0 p-2   "
+            >
+              <ImCross />
+            </button>
+          </div>
 
-                    {/* Back Side */}
-                    <div className="back w-full h-full flex flex-col justify-center items-center py-8 gap-6 bg-white rounded-lg shadow-md absolute rotate-y-180 backface-hidden">
-                        <p className="text-[0.6rem] font-bold">Sumu Clothing</p>
-                        <div className="h-[75px] w-[75px]">
-                            <img src={MyLogo} alt="logo" />
-                        </div>
-                        <p className="text-[0.6rem]">Don't Have an Account?</p>
-                        <button className="text-center text-[0.6rem] border-2 rounded-lg border-[purple] px-3 py-1">
-                            Signup
-                        </button>
-                    </div>
-                </div>
-            </div>
+          {/* Back Side */}
+          <div className="back w-full   h-full flex flex-col justify-center items-center py-8 gap-6 bg-white rounded-lg shadow-md relative rotate-y-180 backface-hidden">
+            <p className="text-[0.6rem] font-bold">Sumu Clothing</p>
+            <form className="flex flex-col items-center   w-[90%] ">
+              <input
+                type="text"
+                placeholder="Enter your Email"
+                className=" text-[0.8rem] w-full border-2 border-[var(--primary-bg-color)] py-1 pl-4 rounded-md"
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="password"
+                className="border-2 text-[0.8rem] w-full border-[var(--primary-bg-color)] py-1 pl-4 rounded-md"
+              />
+              <div className="flex gap-4 py-4 items-center ">
+                <input type="checkbox" className="h-5 w-5" />
+                <p className="text-[0.8rem]">Remember me</p>
+              </div>
+              <button
+                type="submit"
+                className="text-center text-[0.6rem] border-2 rounded-lg border-[purple] px-3 py-1"
+              >
+                Signup
+              </button>
+            </form>
+            <p className="text-[0.6rem]">Have an Account?</p>
+            <button className="text-center text-[0.6rem] border-2 rounded-lg border-[purple] px-3 py-1">
+              login
+            </button>
+            <button
+              onClick={HideShow}
+              className="absolute top-0 right-0 p-2   "
+            >
+              <ImCross />
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import App from "./App";
 import FlipCard from "./components/FlipCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  // <React.StrictMode>
-  <FlipCard />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
